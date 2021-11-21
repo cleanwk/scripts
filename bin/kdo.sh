baa7b15dc9bc9f0be0e200565fa0bf8f9bc240fd
wukai <wuk.jlu@outlook.com>
2021-11-21 16:16:10 +0800

    init git repo operation added

--- bin/kdo.sh
+++ bin/kdo.sh
@@ -1,9 +1,10 @@
 #封装常用操作，提高效率
 ARG=$1
+REMOTE_ADDRESS=$2
 
 git_operation() {
     #绿色字
-    echo "\033[32m enter you commit message please: \n \033[0m"
+    echo "\033[32m ------>>>  enter you commit message please: \n \033[0m"
     read message
     git add .
     git commit -m "$message"
@@ -12,7 +13,21 @@ git_operation() {
     echo "\033[32m ------>>>  all commit push to your repo successfully! \033[0m"
 
 }
+git_init() {
+    #绿色字
+    echo "\033[32m ------>>>  enter you first line in readme.md: \n \033[0m"
+    read message
+    echo "$message" >>README.md
+    echo "\033[32m ------>>>  readme.md create successfully!: \n \033[0m"
+    git init
+    git add .
+    git commit -m "init repo"
+    git remote add origin $REMOTE_ADDRESS
+    git push -u origin master
 
+    #绿色字体
+    echo "\033[32m ------>>>  init repo && push to your repo successfully! \033[0m"
+}
 #*********************************main task ************************************
 
 case $ARG in
@@ -24,6 +39,12 @@ case $ARG in
     git_operation
     ;;
 
+'-ir')
+    git_init
+    ;;
+'--initrepo')
+    git_init
+    ;;
 *)
     #红色字体
     echo "\033[31m ------>>>  illegal args operation failed ！ \033[0m"
